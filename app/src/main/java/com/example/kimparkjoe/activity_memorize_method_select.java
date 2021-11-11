e3956cc1a1d1f62e856d2e121fe2c34bac931cfe
ParkJinu <jinu200131@gmail.com>
2021-11-11 15:34:09 +0900

    11/11 암기 탭 메인 레이아웃

---
+++ app/src/main/java/com/example/kimparkjoe/activity_memorize_method_select.java
@@ -0,0 +1,14 @@
+package com.example.kimparkjoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_memorize_method_select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memorize_method_select);
+    }
+}
