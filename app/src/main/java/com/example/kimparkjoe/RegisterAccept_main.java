59fa63f1fb833bd55342aac8e107886acd90e16b
JaeYoung_Joe <whwodud0303@naver.com>
2021-11-13 16:10:13 +0900

    11/13 회원가입 정보 완료 안내 창

---
+++ app/src/main/java/com/example/kimparkjoe/RegisterAccept_main.java
@@ -0,0 +1,14 @@
+package com.example.kimparkjoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterAccept_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_accept_main);
+    }
+}
