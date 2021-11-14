7a694705b0976be656e4ccdfd7da7ae705160272
jinu200131@gmail.com <jinu200131@gmail.com>
2021-11-14 23:34:20 +0900

    11/14 암기탭 하위 레이아웃 작성 (암기 방식 선택, 목록형, 터치형, 다지선다형)

---
+++ app/src/main/java/com/example/kimparkjoe/memorize_select.java
@@ -0,0 +1,14 @@
+package com.example.kimparkjoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class memorize_select extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memorize_select);
+    }
+}
