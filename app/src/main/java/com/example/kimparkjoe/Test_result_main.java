50b0179a1d62a9b8fb7830f3cdff3f29eb4fbd07
JaeYoung_Joe <whwodud0303@naver.com>
2021-11-16 12:17:35 +0900

    시험 관련 화면들

---
+++ app/src/main/java/com/example/kimparkjoe/Test_result_main.java
@@ -0,0 +1,13 @@
+package com.example.kimparkjoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test_result_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_result_main);
+    }
+}
