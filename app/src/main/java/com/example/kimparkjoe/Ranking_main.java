af439f7033eb57436f4eb8f6b4a53e5d6ee4058b
JaeYoung_Joe <whwodud0303@naver.com>
2021-11-16 11:32:02 +0900

    랭킹화면 구현

--- app/src/main/java/com/example/kimparkjoe/Ranking_main.java
+++ app/src/main/java/com/example/kimparkjoe/Ranking_main.java
@@ -10,7 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Ranking_main extends Fragment {
+public class Ranking_main extends Fragment  {
 
     private View view;
 
