30187a570b258441e7737fbae994e4291e2e04dc
JaeYoung_Joe <whwodud0303@naver.com>
2021-11-16 15:16:16 +0900

    11/16 오답노트 화면 구현

---
+++ app/src/main/java/com/example/kimparkjoe/WrongANS_bookmark_main.java
@@ -0,0 +1,14 @@
+package com.example.kimparkjoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WrongANS_bookmark_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrong_ans_bookmark_main);
+    }
+}
