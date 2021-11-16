3fc3dd851266919ab51f6105a54b239d0cf9b967
JaeYoung_Joe <whwodud0303@naver.com>
2021-11-16 15:39:41 +0900

    11/16 친구관련 레이아웃+팝업

---
+++ app/src/main/java/com/example/kimparkjoe/Friend_addFriend.java
@@ -0,0 +1,14 @@
+package com.example.kimparkjoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Friend_addFriend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_add_friend);
+    }
+}
---
+++ app/src/main/java/com/example/kimparkjoe/Friend_request.java
@@ -0,0 +1,14 @@
+package com.example.kimparkjoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Friend_request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_request);
+    }
+}
