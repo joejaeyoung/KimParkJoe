0d7fb2ff57df8fcb08a477ef904e11d8f371e271
JaeYoung_Joe <whwodud0303@naver.com>
2021-11-22 21:12:13 +0900

    11/22오답노트 화면 전환 to 즐겨찾기, 틀린것

--- app/src/main/java/com/example/kimparkjoe/WrongANS_main.java
+++ app/src/main/java/com/example/kimparkjoe/WrongANS_main.java
@@ -5,20 +5,44 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class WrongANS_main extends Fragment {
 
     private View view;
+    private Button bookmarkButton;
+    private Button wrongButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         System.out.println("오답 메인 전환!");
         view=inflater.inflate(R.layout.activity_wrong_ans_main,container,false);
+
+        bookmarkButton = view.findViewById(R.id.btn_wrong_bookmark);
+        wrongButton = view.findViewById(R.id.btn_wrong_wrong);
+
+        bookmarkButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent1 = new Intent(getActivity(), WrongANS_bookmark_main.class);
+                startActivity(intent1);
+            }
+        });
+
+        wrongButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(getActivity(), WrongANS_wrongquestion_main.class);
+                startActivity(intent2);
+            }
+        });
+
 
         return view;
     }
--- app/src/main/java/com/example/kimparkjoe/WrongANS_wrongquestion_main.java
+++ app/src/main/java/com/example/kimparkjoe/WrongANS_wrongquestion_main.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class WongANS_wrongquestion_main extends AppCompatActivity {
+public class WrongANS_wrongquestion_main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
