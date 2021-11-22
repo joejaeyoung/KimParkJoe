b505a96288b326e1ff1cb070afbefb55338c2cb3
JaeYoung_Joe <whwodud0303@naver.com>
2021-11-22 20:50:06 +0900

    11/22 test주차 선택 넘어감

--- app/src/main/java/com/example/kimparkjoe/Ranking_main.java
+++ app/src/main/java/com/example/kimparkjoe/Ranking_main.java
@@ -5,14 +5,18 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
 
 public class Ranking_main extends Fragment  {
 
     private View view;
+    private Button testStartButton;
 
     @Nullable
     @Override
@@ -20,6 +24,18 @@ public class Ranking_main extends Fragment  {
         System.out.println("랭킹 메인 전환!");
         view=inflater.inflate(R.layout.activity_ranking_main,container,false);
 
+        testStartButton = view.findViewById(R.id.btn_test_start);
+        testStartButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getActivity(), Test_selectweeks_main.class);
+                startActivity(intent);
+            }
+        });
+
+
         return view;
     }
+
+
 }
