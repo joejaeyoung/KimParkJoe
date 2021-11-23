96991aef6b8dda1e4f8a0a5f65400cd203e623f7
JaeYoung_Joe <whwodud0303@naver.com>
2021-11-23 21:18:23 +0900

    11/23 임시커밋

--- app/src/main/java/com/example/kimparkjoe/Test_testing_main.java
+++ app/src/main/java/com/example/kimparkjoe/Test_testing_main.java
@@ -3,8 +3,23 @@ package com.example.kimparkjoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Test_testing_main extends AppCompatActivity {
+    private ArrayList<String> ENG_Data = null;
+    private ArrayList<String> KOR_Data = null;
+    TextView en_word;
+    TextView test_totalNum;
+    TextView test_timer;
+    TextView test_weeks;
+
+
+
+
+    //생성자에서 데이터 리스트 객체를 전달받음.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
