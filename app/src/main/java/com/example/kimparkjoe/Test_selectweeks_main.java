b9aac95b96ac66f0aa89c594daee2b5a8ae7790c
jinu200131@gmail.com <jinu200131@gmail.com>
2021-11-28 20:56:52 +0900

    11/28 랭킹탭_테스트 주차선택

--- app/src/main/java/com/example/kimparkjoe/Test_selectweeks_main.java
+++ app/src/main/java/com/example/kimparkjoe/Test_selectweeks_main.java
@@ -2,13 +2,57 @@ package com.example.kimparkjoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class Test_selectweeks_main extends AppCompatActivity {
+public class Test_selectweeks_main extends AppCompatActivity implements View.OnClickListener {
+
+    public static int curr_week;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_selectweeks_main);
+
+        findViewById(R.id.tv_test_week_2).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_3).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_4).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_1).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_5).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_6).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_7).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_8).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_9).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_10).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_11).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_12).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_13).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_14).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_15).setOnClickListener(this);
+        findViewById(R.id.tv_test_week_16).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+//            case R.id.temp_btn_to_method_select:
+//                ((MainActivity)MainActivity.context_main).putWordsFromDB(1);
+//
+//                getActivity().startActivity(new Intent(getActivity(),Memorize_method_select.class));
+//                break;
+            default:
+                String weekString = (String) ((TextView)view).getText();
+                String[] week_array = weekString.split(" ");
+                curr_week = Integer.parseInt(week_array[1]);
+                ((MainActivity)MainActivity.context_main).putWordsFromDB(curr_week);
+                myStartActivity(Test_testing_main.class);
+        }
+    }
+
+    private void myStartActivity(Class c){
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/kimparkjoe/Test_testing_main.java
+++ app/src/main/java/com/example/kimparkjoe/Test_testing_main.java
@@ -3,23 +3,26 @@ package com.example.kimparkjoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
-public class Test_testing_main extends AppCompatActivity {
+public class Test_testing_main extends AppCompatActivity implements View.OnClickListener {
 
     private ArrayList<String> ENG_list, KOR_list;
+    private ArrayList<Integer> ans_num_list;
+    private TextView wordShown, selection_1, selection_2, selection_3, selection_4;
+    private int position;
+    private boolean currShownIsEng; // true면 영어 띄움
 
-    private TextView en_word;//tv_test_select_word
+    private TextView en_word;   //tv_test_select_word
     private TextView test_totalNum;
     private TextView test_timer;
     private TextView test_weeks;
-    private TextView test_selectFIRST;
-    private TextView test_selectSECOND;
-    private TextView test_selectTHIRD;
-    private TextView test_selectFOURTH;
     private Button selectPre;
     private Button selectNext;
 
@@ -37,6 +40,11 @@ public class Test_testing_main extends AppCompatActivity {
             ENG_list.add(key);
             KOR_list.add(MainActivity.wordMap.get(key));
         }
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
